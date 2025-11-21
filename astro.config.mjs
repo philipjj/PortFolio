4bbf4d9775c7e92f382ed63229eb6b2a010ff825
Jockluak Duop <philipjj70@gmail.com>
2025-11-21 12:14:20 +0300

    update

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  integrations: [vue(), tailwind()],
-  site: 'https://your-vercel-domain.vercel.app'
-});
